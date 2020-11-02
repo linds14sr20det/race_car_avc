c276dbcf2f2245e9373586e7ae8365908c84c2ee
Stephen Lind <stephen.r.lind@gmail.com>
2020-11-02 12:34:04 -0700

    animation saved

--- diagnostics/graph_dodge_output.py
+++ diagnostics/graph_dodge_output.py
@@ -1,29 +1,40 @@
 import ADC0832
 import time
 import math
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 def init():
-    print("Recording 5 seconds of sensor data:")
+    print("Recording sensor data:")
     ADC0832.setup()
 
-def loop():
-    data=[]
-    time_start = time.time()
-    while time.time() < time_start+5:
-        analogVal = ADC0832.getResult()
-        Vr = 5 * float(analogVal) / 255
-        data.append(Vr)
-    f = open("vibdata.csv", "w")
-    converted_list = [str(element) for element in data]
-    joined_string = ",".join(converted_list)
-    f.write(joined_string)
-    f.close()
-
+def animate(i, ys):
+    analogVal = ADC0832.getResult()
+    Vr = 5 * float(analogVal) / 255
+    ys.append(Vr)
+    ys = ys[-x_len:]
+    line.set_ydata(ys)
+    return line,
 
 if __name__ == '__main__':
     init()
     try:
-        loop()
+        # Parameters
+        x_len = 200         # Number of points to display
+        y_range = [0, 5]  # Range of possible Y values to display
+
+        # Create figure for plotting
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        xs = list(range(0, 200))
+        ys = [0] * x_len
+        ax.set_ylim(y_range)
+
+        line, = ax.plot(xs, ys)
+        
+        ani = animation.FuncAnimation(fig, animate, fargs=(ys,), interval=10, blit=True)
+        ani.save('animation.mp4')
+        plt.show()
     except KeyboardInterrupt: 
         ADC0832.destroy()
     print('The end!')
