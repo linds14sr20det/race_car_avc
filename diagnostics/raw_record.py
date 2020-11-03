fc2dd5c8e86d9dacc0b15df40b28677e3364a8ed
Stephen Lind <stephen.r.lind@gmail.com>
2020-11-02 21:10:06 -0700

    fixed frequency

--- diagnostics/raw_record.py
+++ diagnostics/raw_record.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
             analogVal = ADC0832.getResult()
             f.write(str(analogVal))
             f.write("\n")
-            time.sleep(0.0005)
+            time.sleep(0.001)
     except KeyboardInterrupt:
         f.close()
         ADC0832.destroy()
