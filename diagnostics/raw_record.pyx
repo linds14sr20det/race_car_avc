92dc9466e5858b190d29b77131d08bd79d64ded0
Stephen Lind <stephen.r.lind@gmail.com>
2020-11-04 16:44:58 -0700

    cython recording loop

---
+++ diagnostics/raw_record.pyx
@@ -0,0 +1,27 @@
+import os
+import sys
+import time
+import ADC0832
+import math
+
+def init():
+    print("Recording sensor data:")
+    ADC0832.setup()
+
+init()
+try:
+    f = open("raw_vibdata.csv", "a+")
+    f.truncate(0)
+    data = []
+    time_start = time.time()
+    while True:
+        analogVal = ADC0832.getResult()
+        data.append(analogVal)
+        #f.write(str(analogVal))
+        #f.write("\n") 
+except KeyboardInterrupt:
+    f.close()
+    print(time.time()-time_start)
+    print(len(data))
+    ADC0832.destroy()
+print('The end!')
