0651122d78675de2be45b098a6cee426420d754e
Stephen Lind <stephen.r.lind@gmail.com>
2020-11-20 14:00:19 -0700

    ABElectronics ADCDAC supported in pure c

---
+++ pure_c/ABE_ADCDACPi.c
@@ -0,0 +1,210 @@
+/*
+================================================
+ABElectronics UK ADC-DAC Pi
+Version 1.1 Update 08/09/2020
+================================================
+*/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+#include <unistd.h>
+
+
+// local variables
+static const char *adcdevice = "/dev/spidev0.0";
+static const char *dacdevice = "/dev/spidev0.1";
+static uint8_t mode = SPI_MODE_0; // SPI_MODE_0
+static uint32_t adcspeed = 1100000; // SPI ADC bus speed 1.1MHz
+static uint32_t dacspeed = 20000000; // SPI DAC bus speed 20MHz
+
+static uint8_t adctx[] = { 0x01, 0x80, 0x00 }; // transmit buffer for the ADC
+static char adcrx[3]; // receive buffer for the adc;
+static uint8_t dactx[2];
+static int adc; // adc object
+static int dac; // dac object
+
+static double adcrefvoltage = 3.3; // reference voltage for the ADC chip.
+static int dacgain = 1; // gain setting for the DAC chip.
+static double dacvoltage = 2.048; // maximum voltage for the DAC output
+
+int open_adc() {
+	/**
+	* Open the ADC SPI bus channel
+	* This needs to be called before using the DAC
+	*/
+
+	// Open SPI device
+	if ((adc = open(adcdevice, O_RDWR)) < 0)
+		return (0);
+
+	if (ioctl(adc, SPI_IOC_WR_MAX_SPEED_HZ, &adcspeed) == -1)
+		return (0);
+	// Set SPI mode
+	if (ioctl(adc, SPI_IOC_WR_MODE, &mode) == -1)
+		return (0);
+
+	return (1);
+}
+
+void close_adc() {
+	/**
+	* Close the ADC SPI bus channel
+	*/
+	close(adc);
+}
+
+int open_dac() {
+	/**
+	* Open the DAC SPI bus channel
+	* This needs to be called before using the DAC
+	*/
+
+	// Open SPI device
+	if ((dac = open(dacdevice, O_RDWR)) < 0)
+		return (0);
+
+	if (ioctl(dac, SPI_IOC_WR_MAX_SPEED_HZ, &dacspeed) == -1)
+		return (0);
+	// Set SPI mode
+	if (ioctl(dac, SPI_IOC_WR_MODE, &mode) == -1)
+		return (0);
+
+	return (1);
+}
+
+void close_dac() {
+	/**
+	* Close the DAC SPI bus channel
+	*/
+	close(dac);
+}
+
+int read_adc_raw(int channel, int mode) {
+	/**
+	* Read the raw value from the ADC
+	* @param channel -  1 to 8
+	* @param mode -  0 = Single Ended or 1 = Differential
+	* When in differential mode setting channel to 1 will make IN1 = IN+ and IN2 = IN-
+	* When in differential mode setting channel to 2 will make IN1 = IN- and IN2 = IN+
+	* @returns 12 bit value between 0 and 4096
+	*/
+	if (channel == 1) {
+		if (mode == 0) {
+			adctx[1] = 0x80;
+		} else if (mode == 1) {
+			adctx[1] = 0x00;
+		} else {
+			return (0);
+		}
+	} else if (channel == 2) {
+		if (mode == 0) {
+			adctx[1] = 0xC0;
+		} else if (mode == 1) {
+			adctx[1] = 0x40;
+		} else {
+			return (0);
+		}
+	} else {
+		return (0);
+	}
+
+	struct spi_ioc_transfer tr = { .tx_buf = (uintptr_t) adctx, .rx_buf =
+			(uintptr_t) adcrx, .len = 3, .delay_usecs = 0, .speed_hz = adcspeed,
+			.bits_per_word = 8, };
+
+	if (ioctl(adc, SPI_IOC_MESSAGE(1), &tr) == -1){
+		return 0;
+	}
+
+	return (((adcrx[1] & 0x0F) << 8) + (adcrx[2]));
+
+}
+
+double read_adc_voltage(int channel, int mode) {
+	/**
+	* Read the voltage from the ADC
+	* @param channel - 1 or 2
+	* @param mode - 0 = Single Ended or 1 = Differential
+	* When in differential mode setting channel to 1 will make IN1 = IN+ and IN2 = IN-
+	* When in differential mode setting channel to 2 will make IN1 = IN- and IN2 = IN+
+	* @returns between 0V and the reference voltage
+	*/
+
+	int rawval = read_adc_raw(channel, mode);
+	return ((adcrefvoltage / 4096) * (double) rawval);
+}
+
+void set_adc_refvoltage(double ref) {
+	/**
+	* Set the reference voltage for the adc
+	* @param ref - Set this value to be the same as the voltage measured on the 3.3V GPIO pin
+	*/
+	adcrefvoltage = ref;
+}
+
+void set_dac_raw(uint16_t raw, int channel) {
+	/**
+	* Set the raw value from the selected channel on the DAC
+	* @param raw - between 0 and 4095
+	* @param channel - 1 or 2
+	*/
+
+
+	dactx[1] = (raw & 0xff);
+	dactx[0] = (((raw >> 8) & 0xff) | (channel - 1) << 7 | 0x1 << 5 | 1 << 4);
+
+	if (dacgain == 2) {
+		uint8_t x = dactx[0];
+		dactx[0] = (x &= ~(1 << 5));
+    }
+
+	struct spi_ioc_transfer tr = { .tx_buf = (unsigned long) &dactx, .rx_buf =
+			(unsigned long) NULL, .len = 2, .delay_usecs = 0, .speed_hz = dacspeed,
+			.bits_per_word = 8, .cs_change = 0, };
+
+	// Write data
+	if (ioctl(dac, SPI_IOC_MESSAGE(1), &tr) < 1) {
+		return;
+	}
+}
+
+void set_dac_voltage(double voltage, int channel) {
+	/**
+	* Set the DAC voltage
+	* @param voltage - between 0 and 2.048 when gain is set to 1,  0 and 3.3 when gain is set to 2
+	* @param channel - 1 or 2
+	*/
+	if (channel == 1) {
+		adctx[1] = 0x80;
+	} else if (channel == 2) {
+		adctx[1] = 0xC0;
+	} else {
+		return;
+	}
+	if ((voltage >= 0.0) && (voltage < dacvoltage)) {
+		uint16_t rawval = ((voltage / 2.048) * 4096) / dacgain;
+		set_dac_raw(rawval, channel);
+	} else {
+		return;
+	}
+}
+
+void set_dac_gain(int gain) {
+	/**
+	* Set the DAC gain
+	* @param gain - 1 or 2 - The output voltage will be between 0 and 2.048V when gain is set to 1,  0 and 3.3V when gain is set to 2
+	*/
+	if (gain == 1) {
+		dacgain = 1;
+		dacvoltage = 2.048;
+	}
+	if (gain == 2) {
+		dacgain = 2;
+		dacvoltage = 3.3;
+	}
+
+}
+
--- pure_c/avc.c
+++ pure_c/avc.c
@@ -1,85 +1,31 @@
-#include <wiringPi.h>
 #include <stdio.h>
-#include <math.h>
-#include <time.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-typedef unsigned char uchar;
-typedef unsigned int uint;
+#include "ABE_ADCDACPi.h"
 
-#define     ADC_CS    0
-#define     ADC_CLK   1
-#define     ADC_DIO   2
+int main(int argc, char **argv){
+	setvbuf (stdout, NULL, _IONBF, 0); // needed to print to the command line
 
-uchar get_ADC_Result(void)
-{
-	uchar i;
-	uchar dat1=0, dat2=0;
-
-	digitalWrite(ADC_CS, 0);
-	digitalWrite(ADC_CLK,0);
-	digitalWrite(ADC_DIO,1);	delayMicroseconds(2);
-	digitalWrite(ADC_CLK,1);	delayMicroseconds(2);
-
-	digitalWrite(ADC_CLK,0);
-	digitalWrite(ADC_DIO,1);    delayMicroseconds(2);
-	digitalWrite(ADC_CLK,1);	delayMicroseconds(2);
-
-	digitalWrite(ADC_CLK,0);
-	digitalWrite(ADC_DIO,0);	delayMicroseconds(2);
-	digitalWrite(ADC_CLK,1);
-	digitalWrite(ADC_DIO,1);    delayMicroseconds(2);
-	digitalWrite(ADC_CLK,0);
-	digitalWrite(ADC_DIO,1);    delayMicroseconds(2);
-
-	for(i=0;i<8;i++)
-	{
-		digitalWrite(ADC_CLK,1);	delayMicroseconds(2);
-		digitalWrite(ADC_CLK,0);    delayMicroseconds(2);
-
-		pinMode(ADC_DIO, INPUT);
-		dat1=dat1<<1 | digitalRead(ADC_DIO);
+	if (open_adc() != 1){ // open the ADC spi channel
+		printf("Failed to open the ADC.\n");	
+		exit(1); // if the SPI bus fails to open exit the program
 	}
 
-	for(i=0;i<8;i++)
-	{
-		dat2 = dat2 | ((uchar)(digitalRead(ADC_DIO))<<i);
-		digitalWrite(ADC_CLK,1); 	delayMicroseconds(2);
-		digitalWrite(ADC_CLK,0);    delayMicroseconds(2);
+	if (open_dac() != 1){ // open the DAC spi channel
+		printf("Failed to open the DAC.\n");
+		exit(1); // if the SPI bus fails to open exit the program
 	}
 
-	digitalWrite(ADC_CS,1);
+	set_dac_gain(2);
 
-	return(dat1==dat2) ? dat1 : 0;
-}
-
-int main(void)
-{
-	uchar analogVal;
-	double Vr, Rt, temp;
-
-	if(wiringPiSetup() == -1){ //when initialize wiring failed,print messageto screen
-		printf("setup wiringPi failed !");
-		return 1;
+	while (1){
+		// Set the output voltage of channel 2 to the input voltage of channel 1
+		set_dac_voltage(read_adc_voltage(1, 0), 2);
 	}
 
-	pinMode(ADC_CS,  OUTPUT);
-	pinMode(ADC_CLK, OUTPUT);
-	
-	struct timespec tim;
-   	tim.tv_sec = 0;
-   	tim.tv_nsec = 1000;
-	FILE *fptr;
-	fptr = fopen("output.txt","w");
-	clock_t t;
-	t = clock();
-	while(1){
-		pinMode(ADC_DIO, OUTPUT);
-
-		analogVal = get_ADC_Result();
-		fprintf(fptr, "%d\n", analogVal);
-		clock_nanosleep(CLOCK_REALTIME, 0, &tim, NULL);
-	}
-	printf("Elapsed time: %f", clock() - t);
-	fclose(fptr);
-	return 0;
+	(void)argc;
+	(void)argv;
+	return (0);
 }
