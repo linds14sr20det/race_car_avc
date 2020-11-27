029ed9be69a3d4e05d199163538a3b0dc7803bb5
Stephen Lind <stephen.r.lind@gmail.com>
2020-11-26 21:12:15 -0700

    order

--- avc.cpp
+++ avc.cpp
@@ -23,41 +23,6 @@ void clearscreen()
 	printf("\033[2J\033[1;1H");
 }
 
-int main(int argc, char **argv)
-{
-	setvbuf(stdout, NULL, _IONBF, 0); // needed to print to the command line
-
-	ADCDACPi adcdac;
-
-	if (adcdac.open_adc() != 1)
-	{				// open the ADC spi channel
-		return (1); // if the SPI bus fails to open exit the program
-	}
-
-	ofstream myfile;
-	myfile.open("log.txt");
-	float voltage;
-
-	while (1)
-	{
-		auto begin = std::chrono::high_resolution_clock::now();
-
-		voltage = adcdac.read_adc_voltage(1, 0);
-		auto butterworthed = butterworth(voltage, 6, 20000, 10);
-		myfile << butterworthed << endl;
-
-		auto end = std::chrono::high_resolution_clock::now();
-		auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-		auto elapsed_micro = elapsed.count() * 1e-3;
-		usleep(1000 - elapsed_micro); // sleep 1000 microseconds
-	}
-
-	myfile.close();
-	(void)argc;
-	(void)argv;
-	return (0);
-}
-
 int butterworth(int input, int n, double s, double f)
 {
 	int i = n;
@@ -91,5 +56,40 @@ int butterworth(int input, int n, double s, double f)
 	}
 	return x;
 
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	setvbuf(stdout, NULL, _IONBF, 0); // needed to print to the command line
+
+	ADCDACPi adcdac;
+
+	if (adcdac.open_adc() != 1)
+	{				// open the ADC spi channel
+		return (1); // if the SPI bus fails to open exit the program
+	}
+
+	ofstream myfile;
+	myfile.open("log.txt");
+	float voltage;
+
+	while (1)
+	{
+		auto begin = std::chrono::high_resolution_clock::now();
+
+		voltage = adcdac.read_adc_voltage(1, 0);
+		auto butterworthed = butterworth(voltage, 6, 20000, 10);
+		myfile << butterworthed << endl;
+
+		auto end = std::chrono::high_resolution_clock::now();
+		auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+		auto elapsed_micro = elapsed.count() * 1e-3;
+		usleep(1000 - elapsed_micro); // sleep 1000 microseconds
+	}
+
+	myfile.close();
+	(void)argc;
+	(void)argv;
 	return (0);
 }
