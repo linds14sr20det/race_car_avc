3655238146f98a45e3330b2e9259ad5b8b74f528
Stephen Lind <stephen.r.lind@gmail.com>
2020-12-07 21:53:29 -0700

    updated to use proper timing

--- secondary_path.cpp
+++ secondary_path.cpp
@@ -17,6 +17,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
+#include <thread>
 #include <pthread.h>
 
 #include "ABE_ADCDACPi.h"
@@ -24,7 +25,6 @@
 using namespace std;
 using namespace ABElectronics_CPP_Libraries;
 using Clock = std::chrono::system_clock;
-using Duration = Clock::duration;
 using nanoseconds = std::chrono::nanoseconds;
 
 void *GenerateNoise(void *threadid) {
@@ -38,21 +38,16 @@ void *GenerateNoise(void *threadid) {
 
 	uint16_t DACLookup[16] = {2048,2831,3495,3939,4095,3939,3495,2831,2048,1264,600,156,0,156,600,1264};
 	uint16_t i;
-	struct timespec delay;
-	nanoseconds full_delay = 7407407ns;
+	nanoseconds full_delay = 462962ns;
 
 	while (1) {
 		//We're going to produce a 135Hz wave.
 		//We have 16 points per cycle. Each cycle needs to complete in 7407407 nanoseconds (1e+9/135=7407407)
 		//This means each point needs to run in 462962 nanoseconds.
 		for (i=0; i < 16; i = i + 1) {
-			auto begin = Clock::now();
+			auto next = Clock::now() + full_delay;
 			adcdac.set_dac_raw(DACLookup[i], 1);
-			auto end = Clock::now();
-			auto elapsed = end - begin;
-			delay.tv_sec = 0;
-			delay.tv_nsec = (full_delay - elapsed).count();
-			nanosleep(&delay, NULL);
+			this_thread::sleep_until(next);
 		}
 		i = 0;
 	}
@@ -72,23 +67,17 @@ void *RecordNoise(void *threadid) {
 	ofstream tmpfile;
 	tmpfile.open("log.txt");
 	float voltage;
-	struct timespec delay;
 	nanoseconds full_delay = 1000000ns;
 	while (1) {
 		//We need to have a steady sample rate so we can draw conclusions about the time series
 		//We are going to sampe at 1000Hz.
 		//1/1000=0.001=1000 microseconds 
-		auto begin = Clock::now();
+		auto next = Clock::now() + full_delay;
 
 		voltage = adcdac.read_adc_voltage(1, 0);
 		tmpfile << voltage << endl;
 
-		auto end = Clock::now();
-		auto elapsed = end - begin;
-		delay.tv_sec = 0;
-                delay.tv_nsec = full_delay - elapsed;
-                nanosleep(&delay, NULL);
-
+		this_thread::sleep_until(next);
 		i++;
 		//This thing is sampling really fast and writing to a file. This could corrupt system memory if it gets too big
 		if(i>100000) {
@@ -101,7 +90,6 @@ void *RecordNoise(void *threadid) {
 }
 
 int main(int argc, char **argv) {
-	std::cout << Duration::period::num << " , " << Duration::period::den << '\n';
 	//This program spawns thread to keep things fast
 	pthread_t threads[2];
 
