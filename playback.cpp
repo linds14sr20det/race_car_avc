67a5354212a67f7132d646202d3685c47ffdc6e1
Stephen Lind <stephen.r.lind@gmail.com>
2020-12-20 19:16:02 -0700

    playback in

---
+++ playback.cpp
@@ -0,0 +1,70 @@
+/*
+ *
+ *      compile with "g++ playback.cpp ABE_ADCDACPi.cpp -Wall -Wextra -Wpedantic -Woverflow -o playback"
+ *      run with "./avc"
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdexcept>
+#include <time.h>
+#include <unistd.h>
+#include <iostream>
+#include <chrono>
+#include <fstream>
+#include <math.h>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <random>
+
+#include "ABE_ADCDACPi.h"
+
+using namespace std;
+using namespace ABElectronics_CPP_Libraries;
+using Clock = std::chrono::system_clock;
+using nanoseconds = std::chrono::nanoseconds;
+
+float convert_raw_to_voltage(int raw) 
+{
+	return float(raw) * 3.3 / 4095;
+}
+
+int main(int argc, char **argv)
+{
+	ADCDACPi adcdac;
+
+	if (adcdac.open_dac() != 1)
+	{						// open the DAC spi channel
+		pthread_exit(NULL); // if the SPI bus fails to open exit the program
+	}
+
+	adcdac.set_dac_gain(2); // set the DAC gain to 2 which will give a max voltage of 3.3V
+
+	/* Creating input filestream */
+    	ifstream output_file("output.txt");
+	string line;	
+
+	nanoseconds full_delay = 1000000ns;
+
+	auto start = Clock::now();
+	while (getline(output_file, line))
+	{
+		//We need to have a steady sample rate so we can draw conclusions about the time series
+		//We are going to sample at 1000Hz.
+		//1/1000=0.001=1000 microseconds
+		auto next = Clock::now() + full_delay;
+		
+		adcdac.set_dac_voltage(stof(line), 1); // output anti vibration
+
+		this_thread::sleep_until(next);
+	}
+	tmpfile.close();
+	adcdac.close_adc();
+	adcdac.close_dac();
+
+	(void)argc;
+	(void)argv;
+	return 0;
+}
