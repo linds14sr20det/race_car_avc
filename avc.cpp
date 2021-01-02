2dc7a03c8509fd0db8ea3258577ee7883833966a
Stephen Lind <stephen.r.lind@gmail.com>
2021-01-02 16:45:30 -0700

    initial fine tuning of pid

--- avc.cpp
+++ avc.cpp
@@ -73,7 +73,7 @@ void ActiveVibrationControl()
 	int log_count = 0;
 
 	ADCDACPi adcdac;
-	MiniPID pid = MiniPID(0, 0, 1);
+	MiniPID pid = MiniPID(0.1, 0, 0.01);
 	pid.setOutputLimits(-1.64, 1.64);
 	pid.setOutputRampRate(10);
 
